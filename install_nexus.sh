d708d7dfbfc1724716aa734cecb7847887a8d587
Piero Toffanin <pt@masseranolabs.com>
2022-08-28 13:58:58 +0200

    Fix install_nexus.sh

--- install_nexus.sh
+++ install_nexus.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-curl -L https://github.com/DroneDB/libnexus/releases/download/v1.0.0/nxs-ubuntu-22.04-$(arch).deb --output /tmp/nxs-ubuntu-22.04-$(arch).deb
-dpkg-deb -x /tmp/nxs-ubuntu-22.04-$(arch).deb /usr
-rm /tmp/nxs-ubuntu-22.04-$(arch).deb
+ARCH=$(arch)
+if [[ "$ARCH" = "x86_64" ]]; then
+    ARCH="amd64"
+fi
+
+curl -L https://github.com/DroneDB/libnexus/releases/download/v1.0.0/nxs-ubuntu-22.04-$ARCH.deb --output /tmp/nxs-ubuntu-22.04-$ARCH.deb
+dpkg-deb -x /tmp/nxs-ubuntu-22.04-$ARCH.deb /usr
+rm /tmp/nxs-ubuntu-22.04-$ARCH.deb
